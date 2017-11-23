2c731c514ae67420ff2484f82646f40b423a751c
Rebeca <rramirezb1@ucenfotec.ac.cr>
2017-11-23 00:55:35 -0600

    terminar conection y manager diagn

--- src/Conections/conectionDiagnosis.java
+++ src/Conections/conectionDiagnosis.java
@@ -1,9 +1,11 @@
 package Conections;
 
+import clinicLibrary.Diagnosis;
+
 /**
  *
  * @author kevin
  */
 public class conectionDiagnosis {
-    
+    public static void saveDiagnosis(Diagnosis pDiagnosis){}
 }
--- src/Managers/managerDiagnosis.java
+++ src/Managers/managerDiagnosis.java
@@ -1,9 +1,28 @@
 package Managers;
 
+import Conections.conectionDiagnosis;
+import clinicLibrary.Diagnosis;
+
 /**
  *
  * @author Becky
  */
 public class managerDiagnosis {
     
+    public static boolean registerPatient (String id, String name, String description)
+    {
+        boolean validation;
+        
+        Diagnosis newDiagnosis = new Diagnosis(id, name, description);
+        
+        try{
+            conectionDiagnosis.saveDiagnosis(newDiagnosis);
+            validation = true;
+        }catch(Exception e){
+            validation = false;
+        }
+        
+        
+        return validation;
+    }
 }
