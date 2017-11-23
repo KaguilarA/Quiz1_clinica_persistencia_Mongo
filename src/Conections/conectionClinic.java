bdd3a0b9f6a800e124251f08ca77498aec5de186
Kevin Aguilar <kaguilara@ucenfotec.ac.cr>
2017-11-23 00:38:57 -0600

    gets clinics

--- src/Conections/conectionClinic.java
+++ src/Conections/conectionClinic.java
@@ -6,8 +6,19 @@ package Conections;
  */
 
 import clinicLibrary.Clinic;
+import java.util.ArrayList;
 
 public class conectionClinic {
     
+    // EL ARREGLO HAY QUE CAMBIARLE EL TIPO A CLINIC
+    private static ArrayList <String> allClinics = new ArrayList<>();
+    
     public static void saveClinic(Clinic pClinic){}
+    
+    public static ArrayList<String> getClinics()
+    {
+        allClinics.add("CLINICAS");
+        
+        return allClinics;
+    }
 }
